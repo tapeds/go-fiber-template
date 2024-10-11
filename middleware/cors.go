1da797a3b1b417d8d62a823fb991657b2ce6b888
tapeds <frrllmatthew@gmail.com>
2024-10-11 20:56:13 +0700

    refactor: add cors allowed origin to env

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,13 +1,23 @@
 package middleware
 
 import (
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func CORSMiddleware() fiber.Handler {
+	origins := os.Getenv("ALLOWED_ORIGIN")
+
+	if origins == "" {
+		origins = "*"
+		log.Println("Allowed Origin is not set, defaulting to '*'")
+	}
+
 	return cors.New(cors.Config{
-		AllowOrigins:     "https://example.com, https://another-example.com",
+		AllowOrigins:     origins,
 		AllowCredentials: true,
 		AllowHeaders:     "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With",
 		AllowMethods:     "POST, HEAD, PATCH, OPTIONS, GET, PUT, DELETE",
