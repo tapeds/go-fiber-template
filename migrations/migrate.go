b35056db155de5ac7de65bc011604ac73ea8950e
tapeds <frrllmatthew@gmail.com>
2024-12-08 15:22:59 +0700

    feat: add extension autorun

--- migrations/migrate.go
+++ migrations/migrate.go
@@ -1,11 +1,26 @@
 package migrations
 
 import (
+	"fmt"
+
 	"github.com/tapeds/go-fiber-template/entity"
 	"gorm.io/gorm"
 )
 
 func Migrate(db *gorm.DB) error {
+	queries := []string{
+		`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`,
+	}
+
+	for _, query := range queries {
+		result := db.Exec(query)
+		if result.Error != nil {
+			fmt.Println("Error executing query:", result.Error)
+		} else {
+			fmt.Println("Executed query successfully:", query)
+		}
+	}
+
 	if err := db.AutoMigrate(
 		&entity.User{},
 	); err != nil {
